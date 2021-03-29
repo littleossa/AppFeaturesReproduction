71f44a5ba41256bb6832f6eb26d8a6358259b011
Lil Ossa <littleossa@gmail.com>
2021-03-29 21:40:15 +0900

    Initial Commit

---
+++ AppFeaturesReproduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppFeaturesReproduction
+//
+//  Created by 平岡修 on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
